f0bfc20d00b8e90d89b4c68828c5142d4f28bfb9
neusc <neusc829@163.com>
2020-01-27 08:54:50 +0000

    fix(checkLogin.go): login user return user struct

--- common/checkLogin.go
+++ common/checkLogin.go
@@ -7,19 +7,19 @@ import (
 	"github.com/globalsign/mgo/bson"
 )
 
-func CheckLogin(c *gin.Context) bool {
+func CheckLogin(c *gin.Context) (entity.User, bool) {
 	cookie, err := c.Cookie("uid")
+	user := entity.User{}
 	if err != nil {
-		return false
+		return user, false
 	}
 	if !bson.IsObjectIdHex(cookie) {
 		c.SetCookie(config.Cookie["name"], "", -1, "/", config.Cookie["domain"], false, false)
-		return false
+		return user, false
 	}
-	user := entity.User{}
 	err = config.Session.DB("filemanager").C("users").FindId(bson.ObjectIdHex(cookie)).One(&user)
 	if err != nil {
-		return false
+		return user, false
 	}
-	return true
+	return user, true
 }
